6305ba38372bdedcb4f8f28289a87e792eb091c2
tadanohone <signal2469@gmail.com>
2022-10-22 17:52:26 +0900

    アニメーションの追加
    落葉の落下

--- main.js
+++ main.js
@@ -32,3 +32,17 @@ if (!localStorage.getItem('name')) {
 myButton.onclick = () => {
     setUserName();
 }
+
+// アニメーション
+let targets = document.querySelectorAll('.js-scroll-fall'); //ターゲット要素
+//スクロールイベント
+window.addEventListener('scroll', function () {
+  var scroll = window.scrollY; //スクロール量を取得
+  var windowHeight = window.innerHeight; //画面の高さを取得
+  for (let target of targets) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+    var targetPos = target.getBoundingClientRect().top + scroll + 500; //ターゲット要素の位置を取得
+    if (scroll > targetPos - windowHeight) { //スクロール量 > ターゲット要素の位置
+      target.classList.add('is-animated'); //is-animatedクラスを加える
+    }
+  }
+});
